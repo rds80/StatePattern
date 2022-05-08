257e71802c54bfabc4be7f9dea7ae4cb5f4b7345
rds80 <rippleshah@msn.com>
2022-05-08 00:10:27 -0400

    GumballMachine - updated to leverage states of classes HasQuarterState - created new state

---
+++ src/HasQuarterState.java
@@ -0,0 +1,38 @@
+public class HasQuarterState implements IState {
+    GumballMachine gumballMachine;
+
+    public HasQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("You can't insert another quarter");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("You turned...");
+        gumballMachine.setState(gumballMachine.getSoldState());
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("No gumball dispense");
+    }
+
+    @Override
+    public void refill() {
+
+    }
+
+    public String toString() {
+        return "waiting for turn of crank";
+    }
+}
