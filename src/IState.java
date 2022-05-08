d65d2be7144fd116dcbe431fac06f5ec76e71c72
rds80 <rippleshah@msn.com>
2022-05-07 23:02:25 -0400

    Added interface to have the actions possible.

---
+++ src/IState.java
@@ -0,0 +1,8 @@
+public interface IState {
+    public void insertQuarter();
+    public void ejectQuarter();
+    public void turnCrank();
+    public void dispense();
+
+    public void refill();
+}
