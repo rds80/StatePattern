7a994bdeed0f31985098beb3e4abf17f3c3f9cc9
rds80 <rippleshah@msn.com>
2022-05-08 14:54:01 -0400

    Updated test run.

--- src/GumballMachineTestDrive.java
+++ src/GumballMachineTestDrive.java
@@ -12,22 +12,6 @@ public class GumballMachineTestDrive {
         gumballMachine.insertQuarter();
         gumballMachine.ejectQuarter();
         gumballMachine.turnCrank();
-
-        System.out.println(gumballMachine);
-
-        gumballMachine.insertQuarter();
-        gumballMachine.turnCrank();
-        gumballMachine.insertQuarter();
-        gumballMachine.turnCrank();
-        gumballMachine.ejectQuarter();
-
-        System.out.println(gumballMachine);
-
-        gumballMachine.insertQuarter();
-        gumballMachine.insertQuarter();
-        gumballMachine.turnCrank();
-        gumballMachine.insertQuarter();
-        gumballMachine.turnCrank();
         gumballMachine.insertQuarter();
         gumballMachine.turnCrank();
 
