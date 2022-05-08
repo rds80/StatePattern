e72c9a72b74cc75c21e8c29b7811ba781a6cf410
rds80 <rippleshah@msn.com>
2022-05-08 14:49:50 -0400

    Added conditional logic to call winnerstate when the turnCrank method is called

--- src/HasQuarterState.java
+++ src/HasQuarterState.java
@@ -1,4 +1,7 @@
+import java.util.Random;
+
 public class HasQuarterState implements IState {
+    Random randomWinner = new Random(System.currentTimeMillis());
     GumballMachine gumballMachine;
 
     public HasQuarterState(GumballMachine gumballMachine) {
@@ -19,7 +22,12 @@ public class HasQuarterState implements IState {
     @Override
     public void turnCrank() {
         System.out.println("You turned...");
-        gumballMachine.setState(gumballMachine.getSoldState());
+        int winner = randomWinner.nextInt(10);
+        if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+            gumballMachine.setState(gumballMachine.getWinnerState());
+        } else {
+            gumballMachine.setState(gumballMachine.getSoldState());
+        }
     }
 
     @Override
